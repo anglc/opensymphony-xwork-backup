869a92459c90969a9a2d5f2a0efd6689de003be1
jcarreira <jcarreira@8c1e88eb-08f3-148f-2d68-3308c1931f23>
2003-08-08 06:05:04 +0000

    test for setting name in action context

---
+++ src/test/com/opensymphony/xwork/DefaultActionProxyTest.java
@@ -0,0 +1,30 @@
+/*
+ * Copyright (c) 2002-2003 by OpenSymphony
+ * All rights reserved.
+ */
+package com.opensymphony.xwork;
+
+import com.opensymphony.xwork.config.ConfigurationManager;
+import com.opensymphony.xwork.config.providers.MockConfigurationProvider;
+
+import junit.framework.TestCase;
+
+
+/**
+ * DefaultActionProxyTest
+ * @author Jason Carreira
+ * Created Aug 8, 2003 1:41:45 AM
+ */
+public class DefaultActionProxyTest extends TestCase {
+    //~ Methods ////////////////////////////////////////////////////////////////
+
+    public void setUp() {
+        ConfigurationManager.clearConfigurationProviders();
+        ConfigurationManager.addConfigurationProvider(new MockConfigurationProvider());
+    }
+
+    public void testActionNameIsSet() throws Exception {
+        ActionProxy proxy = ActionProxyFactory.getFactory().createActionProxy("", MockConfigurationProvider.FOO_ACTION_NAME, null);
+        assertEquals(MockConfigurationProvider.FOO_ACTION_NAME, ActionContext.getContext().getName());
+    }
+}
