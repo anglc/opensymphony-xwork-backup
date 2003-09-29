387cb8ba3befcc21b6f9423bd362d2961304d280
savaki <savaki@8c1e88eb-08f3-148f-2d68-3308c1931f23>
2003-09-29 03:02:46 +0000

    *** empty log message ***

---
+++ src/test/com/opensymphony/xwork/util/Owner.java
@@ -0,0 +1,17 @@
+/*
+ * Copyright (c) 2002-2003 by OpenSymphony
+ * All rights reserved.
+ */
+package com.opensymphony.xwork.util;
+
+public class Owner {
+    private Dog dog;
+
+    public Dog getDog() {
+        return dog;
+    }
+
+    public void setDog(Dog dog) {
+        this.dog = dog;
+    }
+}
