cb34101cd8849a28b95e574838c3ddb3061d7511
cameronbraid <cameronbraid@8c1e88eb-08f3-148f-2d68-3308c1931f23>
2003-10-03 06:14:35 +0000

    Fixed problem where chaining interceptor coppied properties from the first action to itself.

--- src/java/com/opensymphony/xwork/interceptor/ChainingInterceptor.java
+++ src/java/com/opensymphony/xwork/interceptor/ChainingInterceptor.java
@@ -28,8 +28,9 @@ public class ChainingInterceptor extends AroundInterceptor {
     protected void before(ActionInvocation invocation) throws Exception {
         OgnlValueStack stack = invocation.getStack();
         CompoundRoot root = stack.getRoot();
-
-        for (Iterator iterator = root.iterator(); iterator.hasNext();) {
+		Iterator iterator = root.iterator();
+		iterator.next();
+        while(iterator.hasNext()) {
             Object o = iterator.next();
             OgnlUtil.copy(o, invocation.getAction(), ActionContext.getContext().getContextMap());
         }
