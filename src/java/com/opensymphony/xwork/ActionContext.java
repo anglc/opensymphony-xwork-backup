f5cefc1a06000c3753aa4e36a0666e639fbf14f4
jcarreira <jcarreira@8c1e88eb-08f3-148f-2d68-3308c1931f23>
2003-12-03 06:14:15 +0000

    ActionContext initializes a new ActionContext and sets it in the ThreadLocal if the ThreadLocal is null.

--- src/java/com/opensymphony/xwork/ActionContext.java
+++ src/java/com/opensymphony/xwork/ActionContext.java
@@ -77,7 +77,11 @@ public class ActionContext {
      */
     public static ActionContext getContext() {
         ActionContext context = (ActionContext) actionContext.get();
-
+        if (context == null) {
+            OgnlValueStack vs = new OgnlValueStack();
+            context = new ActionContext(vs.getContext());
+            setContext(context);
+        }
         return context;
     }
 
