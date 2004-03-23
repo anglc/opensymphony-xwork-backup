5f7f5598a05576e246f057af6a5053286380985a
unkyaku <unkyaku@8c1e88eb-08f3-148f-2d68-3308c1931f23>
2004-03-23 04:18:26 +0000

    Add javadoc.

--- src/java/com/opensymphony/xwork/config/entities/Parameterizable.java
+++ src/java/com/opensymphony/xwork/config/entities/Parameterizable.java
@@ -8,7 +8,12 @@ import java.util.Map;
 
 
 /**
- * Parameterizable
+ * Actions implementing Parameterizable will receive a map of the static
+ * parameters defined in the Action configuration.
+ * <p>
+ * The {@link com.opensymphony.xwork.interceptor.StaticParametersInterceptor}
+ * must be in the Action's interceptor queue for this to work.
+ *
  * @author Jason Carreira
  * Created Jun 2, 2003 9:56:10 PM
  */
