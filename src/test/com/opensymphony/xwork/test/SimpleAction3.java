e5b35aea6ac206aafaaa7194a2e3d07f81fa7f7c
unkyaku <unkyaku@8c1e88eb-08f3-148f-2d68-3308c1931f23>
2004-05-31 13:04:54 +0000

    Remove wrong copyright that got included because of my IDEA template.

--- src/test/com/opensymphony/xwork/test/SimpleAction3.java
+++ src/test/com/opensymphony/xwork/test/SimpleAction3.java
@@ -2,14 +2,6 @@
  * Copyright (c) 2002-2003 by OpenSymphony
  * All rights reserved.
  */
-/*
- * This file is copyright (c) 2001-2004, Board of Trustees of Stanford
- * University.  Created in the laboratory of Professor Russ B. Altman
- * (russ.altman@stanford.edu), Stanford University, Department of
- * Medicine, as part of the NIH PharmGKB knowledge base development
- * effort.  This work is supported by NIH U01GM61374.  Contact
- * help@pharmgkb.org for assistance, questions or suggestions.
- */
 package com.opensymphony.xwork.test;
 
 import com.opensymphony.xwork.SimpleAction;
