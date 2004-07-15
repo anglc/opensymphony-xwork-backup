42b48e8047dbd9ddc6c3d873806f17fdb13d1200
unkyaku <unkyaku@8c1e88eb-08f3-148f-2d68-3308c1931f23>
2004-07-15 00:29:56 +0000

    Change validator to only perform validation if string is non-empty.

--- src/java/com/opensymphony/xwork/validator/validators/EmailValidator.java
+++ src/java/com/opensymphony/xwork/validator/validators/EmailValidator.java
@@ -10,25 +10,26 @@ import com.opensymphony.xwork.validator.ValidationException;
 
 
 /**
- * EmailValidator checks that given field is a String and a valid email address.
- *
- * @author $Author$
- * @version $Revision$
+ * EmailValidator checks that a given String field, if not empty, is a valid email address.
  */
 public class EmailValidator extends FieldValidatorSupport {
     //~ Methods ////////////////////////////////////////////////////////////////
 
     public void validate(Object object) throws ValidationException {
         String fieldName = getFieldName();
-        Object value = this.getFieldValue(fieldName, object);
+        String value = (String) this.getFieldValue(fieldName, object);
 
-        // if there is no value - don't do comparison
-        // if a value is required, a required validator should be added to the field
         if (value == null) {
             return;
+        } else {
+            value = value.trim();
+
+            if (value.length() == 0) {
+                return;
+            }
         }
 
-        if (!(value instanceof String) || !TextUtils.verifyEmail((String) value)) {
+        if (!TextUtils.verifyEmail(value)) {
             addFieldError(fieldName, object);
         }
     }
