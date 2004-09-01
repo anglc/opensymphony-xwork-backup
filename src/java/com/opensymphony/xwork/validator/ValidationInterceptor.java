e59513f858f1a48d5bacc92b0ca363644992078d
billjive <billjive@8c1e88eb-08f3-148f-2d68-3308c1931f23>
2004-09-01 18:29:54 +0000

    Docs improvements

--- src/java/com/opensymphony/xwork/validator/ValidationInterceptor.java
+++ src/java/com/opensymphony/xwork/validator/ValidationInterceptor.java
@@ -10,24 +10,35 @@ import com.opensymphony.xwork.interceptor.AroundInterceptor;
 
 
 /**
- * ValidationInterceptor
- * <p/>
- * Created : Jan 19, 2003 3:56:42 PM
+ * Validates an action. This interceptor extends the {@link AroundInterceptor} and implements only the
+ * {@link AroundInterceptor#before(com.opensymphony.xwork.ActionInvocation)} method. This class
+ * simply class the {@link ActionValidatorManager#validate(java.lang.Object, java.lang.String)} method
+ * with the given Action and its context.
  *
  * @author Jason Carreira
  */
 public class ValidationInterceptor extends AroundInterceptor {
-    //~ Methods ////////////////////////////////////////////////////////////////
 
+    /**
+     * Does nothing in this implementation.
+     */
     protected void after(ActionInvocation dispatcher, String result) throws Exception {
     }
 
+    /**
+     * Gets the current action and its context and calls
+     * {@link ActionValidatorManager#validate(java.lang.Object, java.lang.String)}.
+     *
+     * @param invocation the execution state of the Action.
+     * @throws Exception if an error occurs validating the action.
+     */
     protected void before(ActionInvocation invocation) throws Exception {
         Action action = invocation.getAction();
         String context = invocation.getProxy().getActionName();
 
         if (log.isDebugEnabled()) {
-            log.debug("Validating " + invocation.getProxy().getNamespace() + "/" + invocation.getProxy().getActionName() + ".");
+            log.debug("Validating "
+                    + invocation.getProxy().getNamespace() + "/" + invocation.getProxy().getActionName() + ".");
         }
 
         ActionValidatorManager.validate(action, context);
