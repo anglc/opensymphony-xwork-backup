88d4a05f6e36d3ef780d51ba5386907466f0c471
plightbo <plightbo@8c1e88eb-08f3-148f-2d68-3308c1931f23>
2005-05-10 01:05:24 +0000

    got rid of System.out crap

--- src/java/com/opensymphony/xwork/util/XWorkListPropertyAccessor.java
+++ src/java/com/opensymphony/xwork/util/XWorkListPropertyAccessor.java
@@ -12,88 +12,85 @@ import java.util.Map;
 
 /**
  * @author Gabriel Zimmerman
- *
- * Overrides the list property accessor so in the case of trying
- * to add properties of a given bean and the JavaBean is not present,
- * this class will create the necessary blank JavaBeans.
+ *         <p/>
+ *         Overrides the list property accessor so in the case of trying
+ *         to add properties of a given bean and the JavaBean is not present,
+ *         this class will create the necessary blank JavaBeans.
  */
 public class XWorkListPropertyAccessor extends ListPropertyAccessor {
-	
-	/* (non-Javadoc)
-	 * @see ognl.PropertyAccessor#getProperty(java.util.Map, java.lang.Object, java.lang.Object)
-	 */
-	public Object getProperty(Map context, Object target, Object name)
-			throws OgnlException {
-		
-		//System.out.println("Entering XWorkListPropertyAccessor. Name: " + name);
-		if (name instanceof Number
-				&& context.get(InstantiatingNullHandler.CREATE_NULL_OBJECTS)!=null){
-					
-			//System.out.println("Getting index from List");
-			List list=(List)target;
-			int index=((Number)name).intValue();
-			int listSize=list.size();
-			if (listSize<=index) {
-				//System.out.println("adding objects to list.");
-				Object result=null;
-				Class lastClass=(Class)context.get(XWorkConverter.LAST_BEAN_CLASS_ACCESSED);
-				String lastProperty=(String)context.get(XWorkConverter.LAST_BEAN_PROPERTY_ACCESSED);
-				if (lastClass==null || lastProperty==null) {
-					return super.getProperty(context, target, name);
-				}
-				Class beanClass=XWorkConverter.getInstance()
-					.getObjectTypeDeterminer().getElementClass(lastClass,lastProperty,name);
-				for (int i=listSize; i<index; i++) {
-					
-					list.add(i, null);
 
-				}
-				try {
-					list.add(index, result=ObjectFactory.getObjectFactory().buildBean(beanClass));
-				}	catch (Exception exc) {
-					
-				}
-				return result;
-			}	else if (list.get(index)==null) {
-				
-			}
-		}
-		return super.getProperty(context, target, name);
-	}
-	
-	
-	/* (non-Javadoc)
-	 * @see ognl.PropertyAccessor#setProperty(java.util.Map, java.lang.Object, java.lang.Object, java.lang.Object)
-	 */
-	public void setProperty(Map context, Object target, Object name, Object value)
-			throws OgnlException {
-		
-		Class lastClass=(Class)context.get(XWorkConverter.LAST_BEAN_CLASS_ACCESSED);
-		String lastProperty=(String)context.get(XWorkConverter.LAST_BEAN_PROPERTY_ACCESSED);
-		Class convertToClass=XWorkConverter.getInstance()
-			.getObjectTypeDeterminer().getElementClass(lastClass,lastProperty,name);
-		
-		Object realValue=getRealValue(context, value, convertToClass);
-		
-		if (target instanceof List && name instanceof Number) {
-			//make sure there are enough spaces in the List to set
-			List list=(List)target;
-			int listSize=list.size();
-			int count=((Number)name).intValue();
-			if (count>=listSize) {
-				for (int i=listSize; i<=count; i++) {
-					list.add(null);
-				}
-			}
-		}
-		
-		super.setProperty(context, target, name, realValue);
-	}
-	
-	private Object getRealValue(Map context, Object value, Class convertToClass) {
-		if (value==null || convertToClass==null) {
-			return value;
-		}
-		return XWorkConverter.getInstance().convertValue(context, value, convertToClass);
-	}
+    /* (non-Javadoc)
+     * @see ognl.PropertyAccessor#getProperty(java.util.Map, java.lang.Object, java.lang.Object)
+     */
+    public Object getProperty(Map context, Object target, Object name)
+            throws OgnlException {
+
+        if (name instanceof Number
+                && context.get(InstantiatingNullHandler.CREATE_NULL_OBJECTS) != null) {
+
+            List list = (List) target;
+            int index = ((Number) name).intValue();
+            int listSize = list.size();
+            if (listSize <= index) {
+                Object result = null;
+                Class lastClass = (Class) context.get(XWorkConverter.LAST_BEAN_CLASS_ACCESSED);
+                String lastProperty = (String) context.get(XWorkConverter.LAST_BEAN_PROPERTY_ACCESSED);
+                if (lastClass == null || lastProperty == null) {
+                    return super.getProperty(context, target, name);
+                }
+                Class beanClass = XWorkConverter.getInstance()
+                        .getObjectTypeDeterminer().getElementClass(lastClass, lastProperty, name);
+                for (int i = listSize; i < index; i++) {
+
+                    list.add(i, null);
+
+                }
+                try {
+                    list.add(index, result = ObjectFactory.getObjectFactory().buildBean(beanClass));
+                } catch (Exception exc) {
+
+                }
+                return result;
+            } else if (list.get(index) == null) {
+
+            }
+        }
+        return super.getProperty(context, target, name);
+    }
+
+
+    /* (non-Javadoc)
+     * @see ognl.PropertyAccessor#setProperty(java.util.Map, java.lang.Object, java.lang.Object, java.lang.Object)
+     */
+    public void setProperty(Map context, Object target, Object name, Object value)
+            throws OgnlException {
+
+        Class lastClass = (Class) context.get(XWorkConverter.LAST_BEAN_CLASS_ACCESSED);
+        String lastProperty = (String) context.get(XWorkConverter.LAST_BEAN_PROPERTY_ACCESSED);
+        Class convertToClass = XWorkConverter.getInstance()
+                .getObjectTypeDeterminer().getElementClass(lastClass, lastProperty, name);
+
+        Object realValue = getRealValue(context, value, convertToClass);
+
+        if (target instanceof List && name instanceof Number) {
+            //make sure there are enough spaces in the List to set
+            List list = (List) target;
+            int listSize = list.size();
+            int count = ((Number) name).intValue();
+            if (count >= listSize) {
+                for (int i = listSize; i <= count; i++) {
+                    list.add(null);
+                }
+            }
+        }
+
+        super.setProperty(context, target, name, realValue);
+    }
+
+    private Object getRealValue(Map context, Object value, Class convertToClass) {
+        if (value == null || convertToClass == null) {
+            return value;
+        }
+        return XWorkConverter.getInstance().convertValue(context, value, convertToClass);
+    }
 }
--- src/java/com/opensymphony/xwork/util/XWorkMapPropertyAccessor.java
+++ src/java/com/opensymphony/xwork/util/XWorkMapPropertyAccessor.java
@@ -15,111 +15,99 @@ import java.util.Map;
 /**
  * Implementation of PropertyAccessor that sets and gets properties by storing and looking
  * up values in Maps.
+ *
  * @author Gabriel Zimmerman
  */
-public class XWorkMapPropertyAccessor extends MapPropertyAccessor
-{
-	private static final String [] INDEX_ACCESS_PROPS=new String [] 
-		{"size", "isEmpty", "keys", "values"};
-	
-	private static final XWorkConverter _converter=XWorkConverter.getInstance();
-	
-	public Object getProperty( Map context, Object target, Object name ) throws OgnlException
-	{
-		// if this is one of the regular index access
-		// properties then just let the superclass deal with the 
-		// get. 
-		if (name instanceof String && contains(INDEX_ACCESS_PROPS, (String)name)) 
-		{
-			System.out.println("returning super.getProperty");
-			return super.getProperty(context, target, name);
-		}
-			
-		Object result=super.getProperty(context,target,name);
-		
-		if (result!=null) {System.out.println(result.toString());}
-		if (result==null) 
-		{
-			System.out.println("result is null");
-			//find the key class and convert the name to that class
-			Class lastClass=(Class)context.get(XWorkConverter.LAST_BEAN_CLASS_ACCESSED);
-			
-			String lastProperty=(String)context.get(XWorkConverter.LAST_BEAN_PROPERTY_ACCESSED);
-			if (lastClass==null || lastProperty==null) {
-				return super.getProperty(context,target,name);
-			}
-			Class keyClass=_converter.getObjectTypeDeterminer()
-			.getKeyClass(lastClass,lastProperty);
-			
-			if(keyClass==null) {
-				
-				keyClass=java.lang.String.class;
-			}
-			System.out.println("key class: " + keyClass.getName());
-			System.out.println("name: " + name);
-			Object key=getKey(context, name);
-			System.out.println("Key (class:value) - "
-					+ key.getClass() + ":"+ key);
-			Map map = (Map)target;
-			result=map.get(key);
-			
-			System.out.println("Result: " + result);
-			if (result==null && 
-				context.get(InstantiatingNullHandler.CREATE_NULL_OBJECTS)!=null)
-			{
-				Class valueClass=_converter.getObjectTypeDeterminer().getElementClass(lastClass, lastProperty,key);
-				
-				try {
-					result=ObjectFactory.getObjectFactory().buildBean(valueClass);
-					map.put(key, result);
-				}	catch (Exception exc) {
-					
-				}
-				
-			}
-		}
-		System.out.println("Result: " + result);
-		return result;
-	}
+public class XWorkMapPropertyAccessor extends MapPropertyAccessor {
+    private static final String[] INDEX_ACCESS_PROPS = new String[]
+    {"size", "isEmpty", "keys", "values"};
 
-	/**
-	 * @param array
-	 * @param name
-	 * @return
-	 */
-	private boolean contains(String[] array, String name) 
-	{
-		for (int i=0; i<array.length; i++) {
-			if (array[i].equals(name)) {
-				return true;
-			}
-		}
-		
-		return false;
-	}
+    private static final XWorkConverter _converter = XWorkConverter.getInstance();
 
-	public void setProperty( Map context, Object target, Object name, Object value ) throws OgnlException
-	{
-		Object key=getKey(context, name);
-		Map map = (Map)target;
-		map.put( key, value );
-	}
-	
-	private Object getKey(Map context, Object name) 
-	{
-		Class lastClass=(Class)context.get(XWorkConverter.LAST_BEAN_CLASS_ACCESSED);
-		String lastProperty=(String)context.get(XWorkConverter.LAST_BEAN_PROPERTY_ACCESSED);
-		if (lastClass==null || lastProperty==null) {
-			return java.lang.String.class;
-		}
-		Class keyClass=_converter.getObjectTypeDeterminer()
-			.getKeyClass(lastClass,lastProperty);
-		if(keyClass==null) {
-			keyClass=java.lang.String.class;
-		}
+    public Object getProperty(Map context, Object target, Object name) throws OgnlException {
+        // if this is one of the regular index access
+        // properties then just let the superclass deal with the
+        // get.
+        if (name instanceof String && contains(INDEX_ACCESS_PROPS, (String) name)) {
+            return super.getProperty(context, target, name);
+        }
 
-		return _converter.convertValue(context, name, keyClass);
-		
-	}
+        Object result = super.getProperty(context, target, name);
+
+        if (result != null) {
+            System.out.println(result.toString());
+        }
+        if (result == null) {
+            //find the key class and convert the name to that class
+            Class lastClass = (Class) context.get(XWorkConverter.LAST_BEAN_CLASS_ACCESSED);
+
+            String lastProperty = (String) context.get(XWorkConverter.LAST_BEAN_PROPERTY_ACCESSED);
+            if (lastClass == null || lastProperty == null) {
+                return super.getProperty(context, target, name);
+            }
+            Class keyClass = _converter.getObjectTypeDeterminer()
+                    .getKeyClass(lastClass, lastProperty);
+
+            if (keyClass == null) {
+
+                keyClass = java.lang.String.class;
+            }
+            Object key = getKey(context, name);
+            Map map = (Map) target;
+            result = map.get(key);
+
+            if (result == null &&
+                    context.get(InstantiatingNullHandler.CREATE_NULL_OBJECTS) != null) {
+                Class valueClass = _converter.getObjectTypeDeterminer().getElementClass(lastClass, lastProperty, key);
+
+                try {
+                    result = ObjectFactory.getObjectFactory().buildBean(valueClass);
+                    map.put(key, result);
+                } catch (Exception exc) {
+
+                }
+
+            }
+        }
+
+        return result;
+    }
+
+    /**
+     * @param array
+     * @param name
+     * @return
+     */
+    private boolean contains(String[] array, String name) {
+        for (int i = 0; i < array.length; i++) {
+            if (array[i].equals(name)) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    public void setProperty(Map context, Object target, Object name, Object value) throws OgnlException {
+        Object key = getKey(context, name);
+        Map map = (Map) target;
+        map.put(key, value);
+    }
+
+    private Object getKey(Map context, Object name) {
+        Class lastClass = (Class) context.get(XWorkConverter.LAST_BEAN_CLASS_ACCESSED);
+        String lastProperty = (String) context.get(XWorkConverter.LAST_BEAN_PROPERTY_ACCESSED);
+        if (lastClass == null || lastProperty == null) {
+            return java.lang.String.class;
+        }
+        Class keyClass = _converter.getObjectTypeDeterminer()
+                .getKeyClass(lastClass, lastProperty);
+        if (keyClass == null) {
+            keyClass = java.lang.String.class;
+        }
+
+        return _converter.convertValue(context, name, keyClass);
+
+    }
 }
 
