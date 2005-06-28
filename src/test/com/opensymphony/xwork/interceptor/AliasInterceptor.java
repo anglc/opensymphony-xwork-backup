a78f46110779ae905a882fec58323bb05ca9659a
matthew_payne <matthew_payne@8c1e88eb-08f3-148f-2d68-3308c1931f23>
2005-06-28 15:15:08 +0000

    Issue number:  XW-289 Obtained from: Matthew Payne Submitted by:  Matthew Payne Reviewed by:
    AliasInterceptor "new"

---
+++ src/test/com/opensymphony/xwork/interceptor/AliasInterceptor.java
@@ -0,0 +1,89 @@
+package com.opensymphony.xwork.interceptor;
+
+import java.util.Iterator;
+import java.util.Map;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
+import com.opensymphony.xwork.ActionContext;
+import com.opensymphony.xwork.ActionInvocation;
+import com.opensymphony.xwork.config.entities.ActionConfig;
+import com.opensymphony.xwork.interceptor.AroundInterceptor;
+import com.opensymphony.xwork.util.OgnlValueStack;
+
+
+
+/** 
+ * The aim of this Interceptor is to covert different named parameters.  
+ * Act as the glue between actions sharing similiar parameter intents, but different naming
+ * It can get helpful when chaining but could also convert request parameters
+ * 
+ * action's aliases expression should be in the form of  #{ "name1" : "alias1", "name2" : "alias2" } 
+ * e.g. assuming action1(or something else in the stack) has a getter named "name1" and the action this interceptor
+ * is applied to has a setter named "alias1", "alias1" will be set with the value from "name1"
+ * 
+ * @author Matthew Payne
+ *
+ */
+
+public class AliasInterceptor extends AroundInterceptor {
+
+    
+    private static final Log log = LogFactory.getLog(AliasInterceptor.class);
+    
+    private static final String DEFAULT_ALIAS_KEY = "aliases" ;
+   
+    String aliasesKey = DEFAULT_ALIAS_KEY;
+    
+    
+    public void setAliasesKey(String aliasesKey) {
+        this.aliasesKey = aliasesKey;
+    }
+    
+    public void destroy() {
+    }
+
+    public void init() {
+    }
+    
+    protected void before(ActionInvocation invocation) throws Exception {
+        ActionConfig config = invocation.getProxy().getConfig();
+        
+        // get the action's parameters
+        final Map parameters = config.getParams(); 
+        
+        if (parameters.containsKey(aliasesKey)) {
+            
+            String aliasExpression = (String) parameters.get(aliasesKey);
+            OgnlValueStack stack = ActionContext.getContext().getValueStack();
+            Object obj = stack.findValue(aliasExpression);
+            
+            if (obj != null && obj instanceof Map) {
+                // override 
+                   Map aliases = (Map)obj;
+                   Iterator itr = aliases.entrySet().iterator();
+                   while(itr.hasNext()) {
+                       Map.Entry entry = (Map.Entry) itr.next();
+                       String name = entry.getKey().toString();
+                       String alias = (String) entry.getValue();
+                       Object value = stack.findValue(name);                 
+                       stack.setValue(alias, value);
+                       
+                   }
+                    
+                } else {
+                    log.debug("invalid alias expression:" +aliasesKey );
+                }
+           
+                
+            }
+             
+    }
+    
+    protected void after(ActionInvocation invocation, String result) throws Exception {
+    }
+    
+    
+
+}
