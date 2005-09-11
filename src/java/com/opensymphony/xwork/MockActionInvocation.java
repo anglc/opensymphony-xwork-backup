e9a46bff2623b9917f73f1a61f67d97768c4d7c6
plightbo <plightbo@8c1e88eb-08f3-148f-2d68-3308c1931f23>
2005-09-11 05:03:26 +0000

    passing tests

---
+++ src/java/com/opensymphony/xwork/MockActionInvocation.java
@@ -0,0 +1,81 @@
+package com.opensymphony.xwork;
+
+import com.opensymphony.xwork.interceptor.PreResultListener;
+import com.opensymphony.xwork.util.OgnlValueStack;
+
+/**
+ * User: plightbo
+ * Date: Sep 10, 2005
+ * Time: 9:53:36 PM
+ */
+public class MockActionInvocation implements ActionInvocation {
+    private Object action;
+    private ActionContext invocationContext;
+    private ActionProxy proxy;
+    private Result result;
+    private String resultCode;
+    private OgnlValueStack stack;
+
+    public Object getAction() {
+        return action;
+    }
+
+    public void setAction(Object action) {
+        this.action = action;
+    }
+
+    public ActionContext getInvocationContext() {
+        return invocationContext;
+    }
+
+    public void setInvocationContext(ActionContext invocationContext) {
+        this.invocationContext = invocationContext;
+    }
+
+    public ActionProxy getProxy() {
+        return proxy;
+    }
+
+    public void setProxy(ActionProxy proxy) {
+        this.proxy = proxy;
+    }
+
+    public Result getResult() {
+        return result;
+    }
+
+    public void setResult(Result result) {
+        this.result = result;
+    }
+
+    public String getResultCode() {
+        return resultCode;
+    }
+
+    public void setResultCode(String resultCode) {
+        this.resultCode = resultCode;
+    }
+
+    public OgnlValueStack getStack() {
+        return stack;
+    }
+
+    public void setStack(OgnlValueStack stack) {
+        this.stack = stack;
+    }
+
+    public boolean isExecuted() {
+        return false;
+    }
+
+    public void addPreResultListener(PreResultListener listener) {
+    }
+
+    public String invoke() throws Exception {
+        return resultCode;
+    }
+
+    public String invokeActionOnly() throws Exception {
+        return resultCode;
+    }
+}
