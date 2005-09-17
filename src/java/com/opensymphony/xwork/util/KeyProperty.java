e1f1a1de35aeb33d446077aa476210ad3bb85717
plightbo <plightbo@8c1e88eb-08f3-148f-2d68-3308c1931f23>
2005-09-17 08:21:47 +0000

    - added java 5.0 syntax to project - added Generics ObjectTypeDeterminer that uses Annotations for the key property

---
+++ src/java/com/opensymphony/xwork/util/KeyProperty.java
@@ -0,0 +1,10 @@
+package com.opensymphony.xwork.util;
+
+/**
+ * User: plightbo
+ * Date: Sep 17, 2005
+ * Time: 1:52:47 AM
+ */
+public @interface KeyProperty {
+    String value() default "id";
+}
