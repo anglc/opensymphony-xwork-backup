8cb8acd5622d1b248fab4ad394b1617ffe6df211
jhouse <jhouse@8c1e88eb-08f3-148f-2d68-3308c1931f23>
2005-09-17 16:41:15 +0000

    Issue number:  XW-299 Obtained from: jhouse Submitted by:  jhouse Reviewed by: CVS: ---------------------------------------------------------------------- CVS: Issue number: CVS:   If this change addresses one or more issues, CVS:   then enter the issue number(s) here. CVS: Obtained from: CVS:   If this change has been taken from another system, CVS:   then name the system in this line, otherwise delete it. CVS: Submitted by: CVS:   If this code has been contributed to the project by someone else; i.e., CVS:   they sent us a patch or a set of diffs, then include their name/email CVS:   address here. If this is your work then delete this line. CVS: Reviewed by: CVS:   If we are doing pre-commit code reviews and someone else has CVS:   reviewed your changes, include their name(s) here. CVS:   If you have not had it reviewed then delete this line.

--- src/java/com/opensymphony/xwork/validator/validators/EmailValidator.java
+++ src/java/com/opensymphony/xwork/validator/validators/EmailValidator.java
@@ -1,36 +1,39 @@
 /*
- * Copyright (c) 2002-2003 by OpenSymphony
+ * Copyright (c) 2002-2005 by OpenSymphony
  * All rights reserved.
  */
 package com.opensymphony.xwork.validator.validators;
 
-import com.opensymphony.util.TextUtils;
-
-import com.opensymphony.xwork.validator.ValidationException;
 
 
 /**
- * EmailValidator checks that a given String field, if not empty, is a valid email address.
+ * EmailValidator checks that a given String field, if not empty, 
+ * is a valid email address.
+ * 
+ * <p>
+ * The regular expression used to validate that the string is an email address
+ * is:
+ * </p>
+ * <pre>
+ * "\\b(^(\\S+@).+((\\.com)|(\\.net)|(\\.org)|(\\.info)|(\\.edu)|(\\.mil)|(\\.gov)|(\\.biz)|(\\.ws)|(\\.us)|(\\.tv)|(\\.cc)|(\\..{2,2}))$)\\b"
+ * </pre>
+ * 
+ * @author jhouse
  */
-public class EmailValidator extends FieldValidatorSupport {
+public class EmailValidator extends StringRegexValidator {
+
+    public static final String emailAddressPattern = 
+        "\\b(^(\\S+@).+((\\.com)|(\\.net)|(\\.org)|(\\.info)|(\\.edu)|(\\.mil)|(\\.gov)|(\\.biz)|(\\.ws)|(\\.us)|(\\.tv)|(\\.cc)|(\\..{2,2}))$)\\b";
+
+    
     //~ Methods ////////////////////////////////////////////////////////////////
 
-    public void validate(Object object) throws ValidationException {
-        String fieldName = getFieldName();
-        String value = (String) this.getFieldValue(fieldName, object);
-
-        if (value == null) {
-            return;
-        } else {
-            value = value.trim();
-
-            if (value.length() == 0) {
-                return;
-            }
-        }
-
-        if (!TextUtils.verifyEmail(value)) {
-            addFieldError(fieldName, object);
-        }
+    public EmailValidator()
+    {
+        setRegex(emailAddressPattern);
+        setCaseSensitive(false);
     }
+
 }
+
+
---
+++ src/java/com/opensymphony/xwork/validator/validators/StringRegexValidator.java
@@ -0,0 +1,90 @@
+/*
+ * Copyright (c) 2002-2005 by OpenSymphony
+ * All rights reserved.
+ */
+package com.opensymphony.xwork.validator.validators;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import com.opensymphony.xwork.validator.ValidationException;
+
+
+/**
+ * StringRegexValidator checks that a given String field, if not empty, 
+ * matches the configured regular expression.
+ * 
+ * @see #setRegex(String)
+ * @see #setCaseSensitive(boolean)
+ * 
+ * @author jhouse
+ */
+public class StringRegexValidator extends FieldValidatorSupport {
+    
+    private String regex = ".";
+    private boolean caseSensitive = true;
+
+    //~ Methods ////////////////////////////////////////////////////////////////
+
+    public void validate(Object object) throws ValidationException {
+        String fieldName = getFieldName();
+        String value = (String) this.getFieldValue(fieldName, object);
+
+        if (value == null) {
+            return;
+        } else {
+            value = value.trim();
+
+            if (value.length() == 0) {
+                return;
+            }
+        }
+        
+        Pattern pattern = null;
+        if(isCaseSensitive())
+            pattern = Pattern.compile(getRegex());
+        else
+            pattern = Pattern.compile(getRegex(), Pattern.CASE_INSENSITIVE);
+
+        Matcher matcher = pattern.matcher(value);
+        
+        if(!matcher.matches()) {
+            addFieldError(fieldName, object);
+        }
+    }
+
+    /**
+     * @return Returns the regular expression to be matched.
+     */
+    public String getRegex() {
+        return regex;
+    }
+
+    /**
+     * Sets the regular expression to be matched.
+     */
+    public void setRegex(String regex) {
+        this.regex = regex;
+    }
+
+    /**
+     * @return Returns whether the expression should be matched against in 
+     * a case-sensitive way.  Default is <code>true</code>.
+     */
+    public boolean isCaseSensitive() {
+        return caseSensitive;
+    }
+
+    /**
+     * Sets whether the expression should be matched against in 
+     * a case-sensitive way.  Default is <code>true</code>.
+     */
+    public void setCaseSensitive(boolean caseSensitive) {
+        this.caseSensitive = caseSensitive;
+    }
+    
+    
+
+}
+
+
