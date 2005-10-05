e9b641bb54c7ca910e764c2e4b2d5e72d4a12473
matthew <matthew@8c1e88eb-08f3-148f-2d68-3308c1931f23>
2005-10-05 01:22:37 +0000

    Issue number:  WW-843

--- src/java/com/opensymphony/xwork/interceptor/ExceptionHolder.java
+++ src/java/com/opensymphony/xwork/interceptor/ExceptionHolder.java
@@ -1,5 +1,9 @@
 package com.opensymphony.xwork.interceptor;
 
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.io.StringWriter;
+
 /**
  * User: Matthew E. Porter (matthew dot porter at metissian dot com)
  * Date: Sep 21, 2005
@@ -15,5 +19,25 @@ public class ExceptionHolder {
     public Exception getException() {
         return this.exception;
     }
-    
+
+    public String getExceptionStack() throws IOException {
+        String exceptionStack = null;
+
+        if (getException() != null) {
+            StringWriter sw = new StringWriter();
+            PrintWriter pw = new PrintWriter(sw);
+
+            try {
+                getException().printStackTrace(pw);
+                exceptionStack = sw.toString();
+            }
+            finally {
+                sw.close();
+                pw.close();
+            }
+        }
+
+        return exceptionStack;
+    }
+
 }
