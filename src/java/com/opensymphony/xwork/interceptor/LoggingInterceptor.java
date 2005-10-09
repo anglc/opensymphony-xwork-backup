a1155fed4583763d8e7f4a08182ebc4693a3ff7a
plightbo <plightbo@8c1e88eb-08f3-148f-2d68-3308c1931f23>
2005-10-09 17:49:08 +0000

    better javadocs

--- src/java/com/opensymphony/xwork/interceptor/LoggingInterceptor.java
+++ src/java/com/opensymphony/xwork/interceptor/LoggingInterceptor.java
@@ -10,34 +10,34 @@ import org.apache.commons.logging.LogFactory;
 
 
 /**
- * LoggingInterceptor
- * <p/>
- * Created : Jan 9, 2003 1:23:03 AM
+ * <!-- START SNIPPET: description -->
+ * This interceptor logs the the start and end of the execution an action (in English-only, not internationalized).
+ * <!-- END SNIPPET: description -->
+ *
+ * <pre>
+ * <!-- START SNIPPET: example -->
+ * &lt;!-- prints out a message before and after the immediate action execution --&gt;
+ * &lt;action name="someAction" class="com.examples.SomeAction"&gt;
+ *     &lt;interceptor-ref name="completeStack"/&gt;
+ *     &lt;interceptor-ref name="logger"/&gt;
+ *     &lt;result name="success"&gt;good_result.ftl&lt;/result&gt;
+ * &lt;/action&gt;
+ *
+ * &lt;!-- prints out a message before any more interceptors continue and after they have finished --&gt;
+ * &lt;action name="someAction" class="com.examples.SomeAction"&gt;
+ *     &lt;interceptor-ref name="logger"/&gt;
+ *     &lt;interceptor-ref name="completeStack"/&gt;
+ *     &lt;result name="success"&gt;good_result.ftl&lt;/result&gt;
+ * &lt;/action&gt;
+ * <!-- END SNIPPET: example -->
+ * </pre>
  *
  * @author Jason Carreira
  */
 public class LoggingInterceptor extends AroundInterceptor {
-
     private static final Log log = LogFactory.getLog(LoggingInterceptor.class);
     private static final String FINISH_MESSAGE = "Finishing execution stack for action ";
     private static final String START_MESSAGE = "Starting execution stack for action ";
-
-
-    public boolean equals(Object o) {
-        if (this == o) {
-            return true;
-        }
-
-        if (!(o instanceof LoggingInterceptor)) {
-            return false;
-        }
-
-        return true;
-    }
-
-    public int hashCode() {
-        return 10;
-    }
 
     protected void after(ActionInvocation invocation, String result) throws Exception {
         logMessage(invocation, FINISH_MESSAGE);
