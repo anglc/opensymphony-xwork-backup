9d8bb280cccb1846570f68f3d27b7eb34acee303
plightbo <plightbo@8c1e88eb-08f3-148f-2d68-3308c1931f23>
2005-10-18 04:46:55 +0000

    more javadocs

--- src/java/com/opensymphony/xwork/util/DefaultObjectTypeDeterminer.java
+++ src/java/com/opensymphony/xwork/util/DefaultObjectTypeDeterminer.java
@@ -3,12 +3,31 @@
  */
 package com.opensymphony.xwork.util;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
 /**
- * ObjectTypeDeterminer that implements the default XWork settings
+ * <!-- START SNIPPET: javadoc -->
+ *
+ * This {@link ObjectTypeDeterminer} looks at the <b>Class-conversion.properties</b> for entries that indicated what
+ * objects are contained within Maps and Collections. For Collections, such as Lists, the element is specified using the
+ * pattern <b>Element_xxx</b>, where xxx is the field name of the collection property in your action or object. For
+ * Maps, both the key and the value may be specified by using the pattern <b>Key_xxx</b> and <b>Element_xxx</b>,
+ * respectively.
+ *
+ * <p/> From WebWork 2.1.x, the <b>Collection_xxx</b> format is still supported and honored, although it is deprecated
+ * and will be removed eventually.
+ *
+ * <!-- END SNIPPET: javadoc -->
+ * 
  *
  * @author Gabriel Zimmerman
+ * @see XWorkListPropertyAccessor
+ * @see XWorkCollectionPropertyAccessor
+ * @see XWorkMapPropertyAccessor
  */
 public class DefaultObjectTypeDeterminer implements ObjectTypeDeterminer {
+    private static final Log LOG = LogFactory.getLog(DefaultObjectTypeDeterminer.class);
 
     public static final String KEY_PREFIX = "Key_";
     public static final String ELEMENT_PREFIX = "Element_";
@@ -17,33 +36,24 @@ public class DefaultObjectTypeDeterminer implements ObjectTypeDeterminer {
 
 
     /**
-     * Determines the key class by looking for the
-     * value of Key_${property} in the properties
-     * file for the given class.
+     * Determines the key class by looking for the value of Key_${property} in the properties file for the given class.
      *
-     * @param parentClass the Class which contains as a property the
-     *                    Map or Collection we are finding the key for.
-     * @param property    the property of the Map or Collection for
-     *                    the given parent class
-     * @see com.opensymphony.xwork.util.ObjectTypeDeterminer#getKeyClass(java.lang.Class, java.lang.String)
+     * @param parentClass the Class which contains as a property the Map or Collection we are finding the key for.
+     * @param property    the property of the Map or Collection for the given parent class
+     * @see ObjectTypeDeterminer#getKeyClass(Class, String)
      */
     public Class getKeyClass(Class parentClass, String property) {
-
         return (Class) XWorkConverter.getInstance()
                 .getConverter(parentClass, KEY_PREFIX + property);
     }
 
     /**
-     * Determines the key class by looking for the
-     * value of Element_${property} in the properties
-     * file for the given class. Also looks for the
-     * deprecated Collection_${property}
+     * Determines the key class by looking for the value of Element_${property} in the properties file for the given
+     * class. Also looks for the deprecated Collection_${property}
      *
-     * @param parentClass the Class which contains as a property the
-     *                    Map or Collection we are finding the key for.
-     * @param property    the property of the Map or Collection for
-     *                    the given parent class
-     * @see com.opensymphony.xwork.util.ObjectTypeDeterminer#getElementClass(Class, String, Object)
+     * @param parentClass the Class which contains as a property the Map or Collection we are finding the key for.
+     * @param property    the property of the Map or Collection for the given parent class
+     * @see ObjectTypeDeterminer#getElementClass(Class, String, Object)
      */
     public Class getElementClass(Class parentClass, String property, Object key) {
         Class clazz = (Class) XWorkConverter.getInstance()
@@ -51,17 +61,19 @@ public class DefaultObjectTypeDeterminer implements ObjectTypeDeterminer {
         if (clazz == null) {
             clazz = (Class) XWorkConverter.getInstance()
                     .getConverter(parentClass, DEPRECATED_ELEMENT_PREFIX + property);
+
+            if (clazz != null) {
+                LOG.info("The Collection_xxx pattern for collection type conversion is deprecated. Please use Element_xxx!");
+            }
         }
         return clazz;
     }
 
     /**
-     * Determines the String key property for a Collection by
-     * getting it from the conversion properties file using the
+     * Determines the String key property for a Collection by getting it from the conversion properties file using the
      * KeyProperty_ prefix. KeyProperty_${property}=somePropertyOfBeansInTheSet
      */
     public String getKeyProperty(Class parentClass, String property) {
-        //TODO
         return (String) XWorkConverter.getInstance()
                 .getConverter(parentClass, KEY_PROPERTY_PREFIX + property);
 
--- src/java/com/opensymphony/xwork/util/ObjectTypeDeterminer.java
+++ src/java/com/opensymphony/xwork/util/ObjectTypeDeterminer.java
@@ -4,15 +4,12 @@
 package com.opensymphony.xwork.util;
 
 /**
- * Determines what the key and and element class
- * of a Map or Collection should be. For Maps, the
- * elements are the values. For Collections, the
- * elements are the elements of the collection.
+ * Determines what the key and and element class of a Map or Collection should be. For Maps, the elements are the
+ * values. For Collections, the elements are the elements of the collection.
  *
  * @author Gabriel Zimmerman
  */
 public interface ObjectTypeDeterminer {
-
     public Class getKeyClass(Class parentClass, String property);
 
     public Class getElementClass(Class parentClass, String property, Object key);
