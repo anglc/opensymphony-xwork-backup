e2207cd02e9a251504051690e9569e242043a46d
rainerh <rainerh@8c1e88eb-08f3-148f-2d68-3308c1931f23>
2005-11-28 18:18:41 +0000

    o added APT test framework   (contributed by Nils Hartmann)

---
+++ tiger/src/test/com/opensymphony/xwork/apt/AptRunnerResult.java
@@ -0,0 +1,62 @@
+/* com.opensymphony.xwork.apt.AptRunnerResult, created Aug 13, 2005 */
+package com.opensymphony.xwork.apt;
+
+public class AptRunnerResult {
+	
+	private final String _command;
+	private final int _result;
+	private final String _output;
+	private final String _error;
+	
+	public String getCommand() {
+		return _command;
+	}
+	public String getError() {
+		return _error;
+	}
+	
+	public boolean containsErrors() {
+        return (_error != null) && (_error.length()>0);
+	}
+	
+	public String getOutput() {
+		return _output;
+	}
+	public int getResult() {
+		return _result;
+	}
+	
+	public boolean succeeded() {
+		return (_result == 0);
+	}
+	
+	public AptRunnerResult(int result, String command, String output, String error) {
+		super();
+		this._result = result;
+		_command = command;
+		_output = output;
+		_error = error;
+	}
+	
+	public String toString() {
+			StringBuffer buffer = new StringBuffer();
+			buffer.append("[AptRunnerResult:");
+			buffer.append(" _command: ");
+			buffer.append(_command);
+			buffer.append(" _result: ");
+			buffer.append(_result);
+			buffer.append(" _output: ");
+			buffer.append(_output);
+			buffer.append(" _error: ");
+			buffer.append(_error);
+			buffer.append("]");
+			return buffer.toString();
+		}
+	
+	
+	
+	
+	
+	
+
+}
