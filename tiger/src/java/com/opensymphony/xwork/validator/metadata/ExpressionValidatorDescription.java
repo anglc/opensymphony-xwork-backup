1785f1bad8ffc0749a9ba62fd3d03dd3210a2ca6
rainerh <rainerh@8c1e88eb-08f3-148f-2d68-3308c1931f23>
2005-11-28 18:36:57 +0000

    o added metadata descriptors for apt based validation config generator

---
+++ tiger/src/java/com/opensymphony/xwork/validator/metadata/ExpressionValidatorDescription.java
@@ -0,0 +1,93 @@
+/*
+ * Copyright (c) 2002-2005 by OpenSymphony
+ * All rights reserved.
+ */
+package com.opensymphony.xwork.validator.metadata;
+
+import java.io.StringWriter;
+import java.io.PrintWriter;
+
+/**
+ * <code>ExpressionValidatorDescription</code>
+ *
+ * @author Rainer Hermanns
+ * @version $Id$
+ */
+public class ExpressionValidatorDescription implements ValidatorDescription {
+
+    public String[] aliasNames;
+    public String expression;
+    public String key;
+    public String message;
+    public boolean shortCircuit;
+
+    /**
+     * Sets the aliasNames of the Action aliases as defined in the xwork.xml configuration.
+     *
+     * @param aliasNames the aliasNames of the Action aliases.
+     */
+    public void setAliasNames(String[] aliasNames) {
+        this.aliasNames = aliasNames;
+    }
+
+    public void setExpression(String expression) {
+        this.expression = expression;
+    }
+
+    public void setKey(String key) {
+        this.key = key;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public void setShortCircuit(boolean shortCircuit) {
+        this.shortCircuit = shortCircuit;
+    }
+
+    /**
+     * Returns the field name to create the validation rule for.
+     *
+     * @return The field name to create the validation rule for
+     */
+    public String getFieldName() {
+        throw new UnsupportedOperationException("ExpressionValidator annotations cannot be applied to fields...");
+    }
+
+    public String asXml() {
+        StringWriter sw = new StringWriter();
+        PrintWriter writer = null;
+
+        try {
+            writer = new PrintWriter(sw);
+
+            if ( shortCircuit) {
+                writer.println("\t<validator type=\"expression\">");
+            } else {
+                writer.println("\t<validator type=\"expression\" short-circuit=\"true\">");
+            }
+
+            writer.println("\t\t<param name=\"expression\">" + expression+ "</param>");
+
+            if ( !"".equals(key)) {
+                writer.println("\t\t<message key=\"" + key + "\">" + message + "</message>");
+            } else {
+                writer.println("\t\t<message>" + message + "</message>");
+            }
+
+            writer.println("\t</validator>");
+
+        } finally {
+            if (writer != null) {
+                writer.flush();
+                writer.close();
+            }
+        }
+        return sw.toString();
+    }
+
+    public boolean isSimpleValidator() {
+        return false;
+    }
+}
