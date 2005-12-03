e62b16ad4ef0b7e2ee12d8f59e23737566c41512
rainerh <rainerh@8c1e88eb-08f3-148f-2d68-3308c1931f23>
2005-12-03 19:33:28 +0000

    o added ActionValidatorManagerFactory to support tiger classes

---
+++ src/java/com/opensymphony/xwork/validator/ActionValidatorManagerFactory.java
@@ -0,0 +1,43 @@
+/*
+ * Copyright (c) 2005, Your Corporation. All Rights Reserved.
+ */
+
+package com.opensymphony.xwork.validator;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import com.opensymphony.util.ClassLoaderUtil;
+
+/**
+ * <code>ActionValidatorManagerFactory</code>
+ *
+ * @author <a href="mailto:hermanns@aixcept.de">Rainer Hermanns</a>
+ * @version $Id$
+ */
+public class ActionValidatorManagerFactory {
+
+    private static final Log LOG = LogFactory.getLog(ActionValidatorManagerFactory.class);
+
+    private static ActionValidatorManager instance = new ActionValidatorManager();
+
+    static {
+        try {
+            Class c = ClassLoaderUtil.loadClass("com.opensymphony.xwork.util.AnnotationActionValidatorManager", ActionValidatorManagerFactory.class);
+
+            LOG.info("Detected AnnotationActionValidatorManager, initializing it...");
+            instance = (ActionValidatorManager) c.newInstance();
+        } catch (ClassNotFoundException e) {
+            // this is fine, just fall back to the default object type determiner
+        } catch (Exception e) {
+            LOG.error("Exception when trying to create new AnnotationActionValidatorManager", e);
+        }
+    }
+
+    public static void setInstance(ActionValidatorManager instance) {
+        ActionValidatorManagerFactory.instance = instance;
+    }
+
+    public static ActionValidatorManager getInstance() {
+        return instance;
+    }
+}
