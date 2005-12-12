cc2bc49fa2c6ca9daab4f7b2a32c3cc37bc78e18
rainerh <rainerh@8c1e88eb-08f3-148f-2d68-3308c1931f23>
2005-12-12 17:40:27 +0000

    WW-1006 annotation docs

--- tiger/src/java/com/opensymphony/xwork/util/KeyProperty.java
+++ tiger/src/java/com/opensymphony/xwork/util/KeyProperty.java
@@ -1,9 +1,48 @@
 package com.opensymphony.xwork.util;
 
 /**
- * User: plightbo
- * Date: Sep 20, 2005
- * Time: 7:41:50 PM
+ * <!-- START SNIPPET: description -->
+ * <p/>Sets the KeyProperty for type conversion.
+ * <!-- END SNIPPET: description -->
+ *
+ * <p/> <u>Annotation usage:</u>
+ *
+ * <!-- START SNIPPET: usage -->
+ * <p/>The KeyProperty annotation must be applied at method level.
+ * <!-- END SNIPPET: usage -->
+ *
+ * <p/> <u>Annotation parameters:</u>
+ *
+ * <!-- START SNIPPET: parameters -->
+ * <table>
+ * <thead>
+ * <tr>
+ * <th>Parameter</th>
+ * <th>Required</th>
+ * <th>Default</th>
+ * <th>Description</th>
+ * </tr>
+ * </thead>
+ * <tbody>
+ * <tr>
+ * <td>id</td>
+ * <td>no</td>
+ * <td>id</td>
+ * <td>The key property value.</td>
+ * </tr>
+ * </tbody>
+ * </table>
+ * <!-- END SNIPPET: parameters -->
+ *
+ * <p/> <u>Example code:</u>
+ *
+ * <pre>
+ * <!-- START SNIPPET: example -->
+ * <!-- END SNIPPET: example -->
+ * </pre>
+ *
+ * @author Patrick Lightbody
+ * @author Rainer Hermanns
  */
 public @interface KeyProperty {
     String value() default "id";
