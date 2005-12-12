f022fe56bd0180fe0ac26769590b620357f9369a
rainerh <rainerh@8c1e88eb-08f3-148f-2d68-3308c1931f23>
2005-12-12 16:23:51 +0000

    WW-1006 annotation docs

--- tiger/src/java/com/opensymphony/xwork/interceptor/annotations/After.java
+++ tiger/src/java/com/opensymphony/xwork/interceptor/annotations/After.java
@@ -7,10 +7,47 @@ package com.opensymphony.xwork.interceptor.annotations;
 import java.lang.annotation.*;
 
 /**
+ * <!-- START SNIPPET: description -->
  * Marks a action method that needs to be called after the main action method and the result was
  * executed. Return value is ignored.
+ * <!-- END SNIPPET: description -->
+ *
+ * <p/> <u>Annotation usage:</u>
+ *
+ * <!-- START SNIPPET: usage -->
+ * The Before annotation can be applied at method level.
+ *
+ * <!-- END SNIPPET: usage -->
+ *
+ * <p/> <u>Annotation parameters:</u>
+ *
+ * <!-- START SNIPPET: parameters -->
+ *
+ * no parameters
+ *
+ * <!-- END SNIPPET: parameters -->
+ *
+ * <p/> <u>Example code:</u>
+ *
+ * <pre>
+ * <!-- START SNIPPET: example -->
+ * public class SampleAction extends ActionSupport {
+ *
+ *  @After
+ *  public void isValid() throws ValidationException {
+ *    // validate model object, throw exception if failed
+ *  }
+ *
+ *  public String execute() {
+ *     // perform action
+ *     return SUCCESS;
+ *  }
+ * }
+ * <!-- END SNIPPET: example -->
+ * </pre>
  *
  * @author Zsolt Szasz, zsolt at lorecraft dot com
+ * @author Rainer Hermanns
  */
 @Retention(RetentionPolicy.RUNTIME)
 @Target({ElementType.METHOD})
