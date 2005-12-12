91be7cc833b508d5a4a2746d73b0a1b79a47b22b
rainerh <rainerh@8c1e88eb-08f3-148f-2d68-3308c1931f23>
2005-12-12 16:05:47 +0000

    WW-1006 annotation docs

--- tiger/src/java/com/opensymphony/xwork/conversion/annotations/TypeConversion.java
+++ tiger/src/java/com/opensymphony/xwork/conversion/annotations/TypeConversion.java
@@ -7,10 +7,8 @@ package com.opensymphony.xwork.conversion.annotations;
 import java.lang.annotation.*;
 
 /**
- * <code>TypeConversion</code>
- *
- * This annotations is used for class and application wide conversion rules.
- *
+ * <!-- START SNIPPET: description -->
+ * <p/>This annotations is used for class and application wide conversion rules.
  * <p>
  * Class wide conversion:<br/>
  * The conversion rules will be assembled in a file called <code>XXXAction-conversion.properties</code>
@@ -22,6 +20,103 @@ import java.lang.annotation.*;
  * The conversion rules will be assembled within the <code>xwork-conversion.properties</code> file within the classpath root.
  * Set type to: <code>type = ConversionType.APPLICATION</code>
  * <p/>
+ * <!-- END SNIPPET: description -->
+ *
+ * <p/> <u>Annotation usage:</u>
+ *
+ * <!-- START SNIPPET: usage -->
+ * The TypeConversion annotation can be applied at property and method level.
+ * <!-- END SNIPPET: usage -->
+ *
+ * <p/> <u>Annotation parameters:</u>
+ *
+ * <!-- START SNIPPET: parameters -->
+ * <table>
+ * <thead>
+ * <tr>
+ * <th>Parameter</th>
+ * <th>Required</th>
+ * <th>Default</th>
+ * <th>Description</th>
+ * </tr>
+ * </thead>
+ * <tbody>
+ * <tr>
+ * <td>key</td>
+ * <td>no</td>
+ * <td>The annotated property/key name</td>
+ * <td>The optional property name mostly used within TYPE level annotations.</td>
+ * </tr>
+ * <tr>
+ * <td>type</td>
+ * <td>no</td>
+ * <td>ConversionType.CLASS</td>
+ * <td>Enum value of ConversionType.  Determines whether the conversion should be applied at application or class level.</td>
+ * </tr>
+ * <tr>
+ * <td>rule</td>
+ * <td>no</td>
+ * <td>ConversionRule.PROPERTY</td>
+ * <td>Enum value of ConversionRule. The ConversionRule can be a property, a Collection or a Map.</td>
+ * </tr>
+ * <tr>
+ * <td>converter</td>
+ * <td>either this or value</td>
+ * <td>&nbsp;</td>
+ * <td>The class name of the TypeConverter to be used as converter.</td>
+ * </tr>
+ * <tr>
+ * <td>value</td>
+ * <td>either converter or this</td>
+ * <td>&nbsp;</td>
+ * <td>The value to set for ConversionRule.KEY_PROPERTY.</td>
+ * </tr>
+ * </tbody>
+ * </table>
+ *
+ * <!-- END SNIPPET: parameters -->
+ *
+ * <p/> <u>Example code:</u>
+ *
+ * <pre>
+ * <!-- START SNIPPET: example -->
+ * @Conversion()
+ * public class ConversionAction implements Action {
+ *
+ *  private String convertInt;
+ *
+ * private String convertDouble;
+ * private List users = null;
+ *
+ * private HashMap keyValues = null;
+ *
+ *   @TypeConversion(type = ConversionType.APPLICATION, converter = "com.opensymphony.xwork.util.XWorkBasicConverter")
+ *   public void setConvertInt( String convertInt ) {
+ *       this.convertInt = convertInt;
+ *   }
+ *
+ *   @TypeConversion(converter = "com.opensymphony.xwork.util.XWorkBasicConverter")
+ *   public void setConvertDouble( String convertDouble ) {
+ *       this.convertDouble = convertDouble;
+ *   }
+ *
+ *   @TypeConversion(rule = ConversionRule.COLLECTION, converter = "java.util.String")
+ *   public void setUsers( List users ) {
+ *       this.users = users;
+ *   }
+ *
+ *   @TypeConversion(rule = ConversionRule.MAP, converter = "java.math.BigInteger")
+ *   public void setKeyValues( HashMap keyValues ) {
+ *       this.keyValues = keyValues;
+ *   }
+ *
+ *   @TypeConversion(type = ConversionType.APPLICATION, property = "java.util.Date", converter = "com.opensymphony.xwork.util.XWorkBasicConverter")
+ *   public String execute() throws Exception {
+ *       return SUCCESS;
+ *   }
+ * }
+ * <!-- END SNIPPET: example -->
+ * </pre>
  *
  * @author Rainer Hermanns
  * @version $Id$
