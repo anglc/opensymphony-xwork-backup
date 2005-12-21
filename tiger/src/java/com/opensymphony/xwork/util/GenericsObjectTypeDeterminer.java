eb0ed021984a64ca2eca91ce89e58ae4bb929ecb
plightbo <plightbo@8c1e88eb-08f3-148f-2d68-3308c1931f23>
2005-12-21 05:17:19 +0000

    - IDEA project cleanup  - make the TypeDeterminer fall back to the default impl if it can't do anything  - also solved a ClassCastException

--- tiger/src/java/com/opensymphony/xwork/util/GenericsObjectTypeDeterminer.java
+++ tiger/src/java/com/opensymphony/xwork/util/GenericsObjectTypeDeterminer.java
@@ -4,6 +4,7 @@ import ognl.OgnlRuntime;
 
 import java.lang.reflect.Field;
 import java.lang.reflect.ParameterizedType;
+import java.lang.reflect.Type;
 import java.util.Map;
 
 /**
@@ -11,9 +12,14 @@ import java.util.Map;
  * Date: Sep 16, 2005
  * Time: 9:58:55 AM
  */
-public class GenericsObjectTypeDeterminer implements ObjectTypeDeterminer {
+public class GenericsObjectTypeDeterminer extends DefaultObjectTypeDeterminer {
     public Class getKeyClass(Class parentClass, String property) {
-        return getClass(parentClass, property, 0);
+        Class clazz = getClass(parentClass, property, 0);
+        if (clazz != null) {
+            return clazz;
+        } else {
+            return super.getKeyClass(parentClass, property);
+        }
     }
 
     public Class getElementClass(Class parentClass, String property, Object key) {
@@ -22,7 +28,12 @@ public class GenericsObjectTypeDeterminer implements ObjectTypeDeterminer {
             index = 1;
         }
 
-        return getClass(parentClass, property, index);
+        Class clazz = getClass(parentClass, property, index);
+        if (clazz != null) {
+            return clazz;
+        } else {
+            return super.getElementClass(parentClass, property, key);
+        }
     }
 
     public String getKeyProperty(Class parentClass, String property) {
@@ -32,13 +43,18 @@ public class GenericsObjectTypeDeterminer implements ObjectTypeDeterminer {
             return annotation.value();
         }
 
-        return null;
+        return super.getKeyProperty(parentClass, property);
     }
 
     private Class getClass(Class parentClass, String property, int index) {
         Field field = OgnlRuntime.getField(parentClass, property);
-        ParameterizedType type = (ParameterizedType) field.getGenericType();
+        Type genericType = field.getGenericType();
+        if (genericType instanceof ParameterizedType) {
+            ParameterizedType type = (ParameterizedType) genericType;
 
-        return (Class) type.getActualTypeArguments()[index];
+            return (Class) type.getActualTypeArguments()[index];
+        } else {
+            return null;
+        }
     }
 }
