45848a28d5754296ccbfc776ffdd141ed14dac4c
rainerh <rainerh@8c1e88eb-08f3-148f-2d68-3308c1931f23>
2006-02-15 21:52:56 +0000

    Add @Key, @Element and @CreateIfNull annotations o updated GenericsObjectTypeDeterminer to handle new annotations Issue number:  XW-337 Obtained from: Submitted by: Reviewed by:

--- tiger/src/java/com/opensymphony/xwork/util/GenericsObjectTypeDeterminer.java
+++ tiger/src/java/com/opensymphony/xwork/util/GenericsObjectTypeDeterminer.java
@@ -8,35 +8,87 @@ import java.lang.reflect.Type;
 import java.util.Map;
 
 /**
- * User: plightbo
- * Date: Sep 16, 2005
- * Time: 9:58:55 AM
+ * GenericsObjectTypeDeterminer
+ *
+ * @author Patrick Lightbody
+ * @author Rainer Hermanns
  */
 public class GenericsObjectTypeDeterminer extends DefaultObjectTypeDeterminer {
+
+    /**
+     * Determines the key class by looking for the value of @Key annotation for the given class.
+     * If no annotation is found, the key class is determined by using the generic parametrics.
+     *
+     * @param parentClass the Class which contains as a property the Map or Collection we are finding the key for.
+     * @param property    the property of the Map or Collection for the given parent class
+     * @see ObjectTypeDeterminer#getKeyClass(Class, String)
+     */
     public Class getKeyClass(Class parentClass, String property) {
-        Class clazz = getClass(parentClass, property, false);
-        if (clazz != null) {
-            return clazz;
-        } else {
-            return super.getKeyClass(parentClass, property);
-        }
-    }
 
-    public Class getElementClass(Class parentClass, String property, Object key) {
-        Class clazz = getClass(parentClass, property, true);
-         if (clazz != null) {
-             return clazz;
-         } else {
-            return super.getElementClass(parentClass, property, key);
-        }
-    }
-
-    public String getKeyProperty(Class parentClass, String property) {
         Field field = OgnlRuntime.getField(parentClass, property);
+
+        if (field != null) {
+            Key annotation = field.getAnnotation(Key.class);
+            if (annotation != null) {
+                return annotation.value();
+            }
+
+            Class clazz = getClass(field, false);
+
+            if (clazz != null) {
+                return clazz;
+            }
+        }
+
+        return super.getKeyClass(parentClass, property);
+    }
+
+    /**
+     * Determines the element class by looking for the value of @Element annotation for the given
+     * class.
+     * If no annotation is found, the element class is determined by using the generic parametrics.
+     *
+     * @param parentClass the Class which contains as a property the Map or Collection we are finding the key for.
+     * @param property    the property of the Map or Collection for the given parent class
+     * @see ObjectTypeDeterminer#getElementClass(Class, String, Object)
+     */
+    public Class getElementClass(Class parentClass, String property, Object key) {
+
+        Field field = OgnlRuntime.getField(parentClass, property);
+
+        if (field != null) {
+            Element annotation = field.getAnnotation(Element.class);
+            if (annotation != null) {
+                return annotation.value();
+            }
+
+            Class clazz = getClass(field, true);
+
+            if (clazz != null) {
+                return clazz;
+            }
+        }
+
+        return super.getElementClass(parentClass, property, key);
+    }
+
+    /**
+     * Determines the key property for a Collection by getting it from the @KeyProperty annotation.
+     *
+     * @param parentClass the Class which contains as a property the Map or Collection we are finding the key for.
+     * @param property    the property of the Map or Collection for the given parent class
+     * @see ObjectTypeDeterminer#getKeyProperty(Class, String)
+     */
+    public String getKeyProperty(Class parentClass, String property) {
+
+        Field field = OgnlRuntime.getField(parentClass, property);
+
         if (field == null) {
             return null;
         }
+
         KeyProperty annotation = field.getAnnotation(KeyProperty.class);
+
         if (annotation != null) {
             return annotation.value();
         }
@@ -44,13 +96,47 @@ public class GenericsObjectTypeDeterminer extends DefaultObjectTypeDeterminer {
         return super.getKeyProperty(parentClass, property);
     }
 
-    private Class getClass(Class parentClass, String property, boolean element) {
+    /**
+     * Determines the createIfNull property for a Collection or Map by getting it from the @CreateIfNull annotation.
+     *
+     * @param parentClass the Class which contains as a property the Map or Collection we are finding the key for.
+     * @param property    the property of the Map or Collection for the given parent class
+     * @param target
+     * @param keyProperty
+     * @param isIndexAccessed <tt>true</tt>, if the collection or map is accessed via index, <tt>false</tt> otherwise.
+     * @see ObjectTypeDeterminer#getKeyProperty(Class, String)
+     */
+    public boolean shouldCreateIfNew(Class parentClass,
+                                     String property,
+                                     Object target,
+                                     String keyProperty,
+                                     boolean isIndexAccessed) {
+
         Field field = OgnlRuntime.getField(parentClass, property);
-        if (field == null) {
-            return null;
+
+        if (field != null) {
+            CreateIfNull annotation = field.getAnnotation(CreateIfNull.class);
+            if (annotation != null) {
+                return annotation.value();
+            }
         }
+
+        return super.shouldCreateIfNew(parentClass, property, target, keyProperty, isIndexAccessed);
+
+    }
+
+    /**
+     * Returns the class for the given field via generic type check.
+     *
+     * @param field The field to check for generic types.
+     * @param element <tt>true</tt> for indexed types and Maps.
+     * @return Class of the specified field.
+     */
+    private Class getClass(Field field, boolean element) {
         Type genericType = field.getGenericType();
+
         if (genericType instanceof ParameterizedType) {
+
             int index = (element && Map.class.isAssignableFrom(field.getType())) ? 1 : 0;
             ParameterizedType type = (ParameterizedType) genericType;
 
