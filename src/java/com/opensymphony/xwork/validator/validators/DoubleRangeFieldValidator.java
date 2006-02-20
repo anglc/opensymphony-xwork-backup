ee585a58cd3123dd0fa6b5ce6e150b35d459a5e0
rainerh <rainerh@8c1e88eb-08f3-148f-2d68-3308c1931f23>
2006-02-20 14:57:33 +0000

    Added inclusive/exclusive min/max values support for DoubleRangeValidator Issue number: Obtained from: Submitted by:  Eugene Schava Reviewed by:   Rainer Hermanns

--- src/java/com/opensymphony/xwork/validator/validators/DoubleRangeFieldValidator.java
+++ src/java/com/opensymphony/xwork/validator/validators/DoubleRangeFieldValidator.java
@@ -4,6 +4,8 @@
  */
 package com.opensymphony.xwork.validator.validators;
 
+import com.opensymphony.xwork.validator.ValidationException;
+
 /**
  * <!-- START SNIPPET: javadoc -->
  * Field Validator that checks if the double specified is within a certain range.
@@ -12,30 +14,39 @@ package com.opensymphony.xwork.validator.validators;
  *
  * <!-- START SNIPPET: parameters -->
  * <ul>
- * 		<li>fieldName - The field name this validator is validating. Required if using Plain-Validator Syntax otherwise not required</li>
- * 		<li>min - the minimum value (if none is specified, it will not be checked) </li>
- * 		<li>max - the maximum value (if none is specified, it will not be checked) </li>
+ *                 <li>fieldName - The field name this validator is validating. Required if using
+Plain-Validator Syntax otherwise not required</li>
+ *                 <li>minInclusive - the minimum inclusive value (if none is specified, it will
+not be checked) </li>
+ *                 <li>maxInclusive - the maximum inclusive value (if none is specified, it will
+not be checked) </li>
+ *                 <li>minExclusive - the minimum exclusive value (if none is specified, it will
+not be checked) </li>
+ *                 <li>maxExclusive - the maximum exclusive value (if none is specified, it will
+not be checked) </li> *
  * </ul>
  * <!-- END SNIPPET: parameters -->
  *
  *
  * <pre>
  * <!-- START SNIPPET: examples -->
- * 		&lt;validators>
+ *                 &lt;validators>
  *           &lt;!-- Plain Validator Syntax --&gt;
  *           &lt;validator type="double">
  *               &lt;param name="fieldName"&gt;percentage&lt;/param&gt;
- *               &lt;param name="min"&gt;20.1&lt;/param&gt;
- *               &lt;param name="max"&gt;50.1&lt;/param&gt;
- *               &lt;message&gt;Age needs to be between ${min} and ${max}&lt;/message&gt;
+ *               &lt;param name="minInclusive"&gt;20.1&lt;/param&gt;
+ *               &lt;param name="maxInclusive"&gt;50.1&lt;/param&gt;
+ *               &lt;message&gt;Age needs to be between ${minInclusive} and
+${maxInclusive} (inclusive)&lt;/message&gt;
  *           &lt;/validator&gt;
  *
  *           &lt;!-- Field Validator Syntax --&gt;
  *           &lt;field name="percentage"&gt;
  *               &lt;field-validator type="double"&gt;
- *                   &lt;param name="min"&gt;0.123&lt;/param&gt;
- *                   &lt;param name="max"&gt;99.98&lt;/param&gt;
- *                   &lt;message&gt;Percentage needs to be between ${min} and ${max}&lt;/message&gt;
+ *                   &lt;param name="minExclusive"&gt;0.123&lt;/param&gt;
+ *                   &lt;param name="maxExclusive"&gt;99.98&lt;/param&gt;
+ *                   &lt;message&gt;Percentage needs to be between ${minExclusive}
+and ${maxExclusive} (exclusive)&lt;/message&gt;
  *               &lt;/field-validator&gt;
  *           &lt;/field&gt;
  *      &lt;/validators&gt;
@@ -45,33 +56,63 @@ package com.opensymphony.xwork.validator.validators;
  * @author Rainer Hermanns
  * @version $Id$
  */
-public class DoubleRangeFieldValidator extends AbstractRangeValidator {
+public class DoubleRangeFieldValidator extends FieldValidatorSupport {
+    
+    Double maxInclusive = null;
+    Double minInclusive = null;
+    Double minExclusive = null;
+    Double maxExclusive = null;
 
-    Double max = null;
-    Double min = null;
+    public void validate(Object object) throws ValidationException {
+        String fieldName = getFieldName();
+        Double value;
+        try {
+            Object obj = this.getFieldValue(fieldName, object);
+            if (obj == null) {
+                return;
+            }
+            value = Double.valueOf(obj.toString());
+        } catch (NumberFormatException e) {
+            return;
+        }
 
-
-    public void setMax(Double max) {
-        this.max = max;
+        if ((maxInclusive != null && value.compareTo(maxInclusive) > 0) ||
+                (minInclusive != null && value.compareTo(minInclusive) < 0) ||
+                (maxExclusive != null && value.compareTo(maxExclusive) >= 0) ||
+                (minExclusive != null && value.compareTo(minExclusive) <= 0)) {
+            addFieldError(fieldName, object);
+        }
     }
 
-    public Double getMax() {
-        return max;
+    public void setMaxInclusive(Double maxInclusive) {
+        this.maxInclusive = maxInclusive;
     }
 
-    public Comparable getMaxComparatorValue() {
-        return max;
+    public Double getMaxInclusive() {
+        return maxInclusive;
     }
 
-    public void setMin(Double min) {
-        this.min = min;
+    public void setMinInclusive(Double minInclusive) {
+        this.minInclusive = minInclusive;
     }
 
-    public Double getMin() {
-        return min;
+    public Double getMinInclusive() {
+        return minInclusive;
     }
 
-    public Comparable getMinComparatorValue() {
-        return min;
+    public Double getMinExclusive() {
+        return minExclusive;
+    }
+
+    public void setMinExclusive(Double minExclusive) {
+        this.minExclusive = minExclusive;
+    }
+
+    public Double getMaxExclusive() {
+        return maxExclusive;
+    }
+
+    public void setMaxExclusive(Double maxExclusive) {
+        this.maxExclusive = maxExclusive;
     }
 }
