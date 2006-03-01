6ad598459464f04d283c865743a124d7f0669404
rgielen <rgielen@8c1e88eb-08f3-148f-2d68-3308c1931f23>
2006-03-01 08:52:06 +0000

    Issue number: XW-345 Obtained from: Submitted by: Claus Ibsen Reviewed by: Rene Gielen test for prepare interceptor

---
+++ src/test/com/opensymphony/xwork/interceptor/PrepareInterceptorTest.java
@@ -0,0 +1,35 @@
+package com.opensymphony.xwork.interceptor;
+
+import com.mockobjects.dynamic.Mock;
+import com.opensymphony.xwork.*;
+import junit.framework.TestCase;
+
+/**
+ * Unit test for PrepareInterceptor.
+ *
+ * @author Claus Ibsen
+ */
+public class PrepareInterceptorTest extends TestCase {
+
+    private Mock mock;
+    private PrepareInterceptor interceptor;
+
+    public void testPrepareCalled() throws Exception {
+        MockActionInvocation mai = new MockActionInvocation();
+        mai.setAction(mock.proxy());
+        mock.expect("prepare");
+
+        interceptor.before(mai);
+        interceptor.after(mai, Action.SUCCESS); // to have higher code coverage
+    }
+
+    protected void setUp() throws Exception {
+        mock = new Mock(Preparable.class);
+        interceptor = new PrepareInterceptor();
+    }
+
+    protected void tearDown() throws Exception {
+        mock.verify();
+    }
+
+}
