c019d835b92736e9b19c96bef543a8fc5aa833db
rainerh <rainerh@8c1e88eb-08f3-148f-2d68-3308c1931f23>
2007-01-06 10:13:49 +0000

    RegexFieldValidator fails matching leading and trailing whitespace
    Issue Number: XW-459

--- src/java/com/opensymphony/xwork2/validator/validators/RegexFieldValidator.java
+++ src/java/com/opensymphony/xwork2/validator/validators/RegexFieldValidator.java
@@ -52,6 +52,7 @@ public class RegexFieldValidator extends FieldValidatorSupport {
 
     private String expression;
     private boolean caseSensitive = true;
+    private boolean trim = true;
 
     public void validate(Object object) throws ValidationException {
         String fieldName = getFieldName();
@@ -81,7 +82,11 @@ public class RegexFieldValidator extends FieldValidatorSupport {
             pattern = Pattern.compile(expression, Pattern.CASE_INSENSITIVE);
         }
 
-        Matcher matcher = pattern.matcher(str);
+        String compare = (String) value;
+        if ( trim ) {
+            compare = compare.trim();
+        }
+        Matcher matcher = pattern.matcher( compare );
 
         if (!matcher.matches()) {
             addFieldError(fieldName, object);
@@ -116,6 +121,22 @@ public class RegexFieldValidator extends FieldValidatorSupport {
      */
     public void setCaseSensitive(boolean caseSensitive) {
         this.caseSensitive = caseSensitive;
+    }
+
+    /**
+     * @return Returns whether the expression should be trimed before matching.
+     * Default is <code>true</code>.
+     */
+    public boolean isTrimed() {
+        return trim;
+    }
+
+    /**
+     * Sets whether the expression should be trimed before matching.
+     * Default is <code>true</code>.
+     */
+    public void setTrim(boolean trim) {
+        this.trim = trim;
     }
 
 }
