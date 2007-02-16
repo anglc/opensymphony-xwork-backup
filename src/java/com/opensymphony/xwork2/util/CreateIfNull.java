98e593efa8a679aefb161768193c0f56aaab3e5c
rainerh <rainerh@8c1e88eb-08f3-148f-2d68-3308c1931f23>
2007-02-16 09:11:20 +0000

    Annotations in package com.opensymphony.xwork(2).util missing Retention annotation Issue Number: XW-480

--- src/java/com/opensymphony/xwork2/util/CreateIfNull.java
+++ src/java/com/opensymphony/xwork2/util/CreateIfNull.java
@@ -5,6 +5,11 @@
 
 package com.opensymphony.xwork2.util;
 
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+import java.lang.annotation.ElementType;
+
 /**
  * <!-- START SNIPPET: description -->
  * <p/>Sets the CreateIfNull for type conversion.
@@ -13,7 +18,7 @@ package com.opensymphony.xwork2.util;
  * <p/> <u>Annotation usage:</u>
  *
  * <!-- START SNIPPET: usage -->
- * <p/>The CreateIfNull annotation must be applied at field level.
+ * <p/>The CreateIfNull annotation must be applied at field or method level.
  * <!-- END SNIPPET: usage -->
  * <p/> <u>Annotation parameters:</u>
  *
@@ -49,6 +54,8 @@ package com.opensymphony.xwork2.util;
  * @author Rainer Hermanns
  * @version $Id$
  */
+@Retention(RetentionPolicy.RUNTIME)
+@Target({ElementType.FIELD, ElementType.METHOD})
 public @interface CreateIfNull {
 
     /**
