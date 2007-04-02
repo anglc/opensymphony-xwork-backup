7489be1071670c044d8ba78ba2eba8639d2d3d25
rainerh <rainerh@8c1e88eb-08f3-148f-2d68-3308c1931f23>
2007-04-02 14:02:20 +0000

    Fixing Unit tests for Locale being de_DE, or fr_FR

--- src/test/com/opensymphony/xwork2/util/LocalizedTextUtilTest.java
+++ src/test/com/opensymphony/xwork2/util/LocalizedTextUtilTest.java
@@ -211,9 +211,17 @@ public class LocalizedTextUtilTest extends XWorkTestCase {
         // This tests will try to load bundles from the 3 locales but we only have files for France and Germany.
         // Before this fix loading the bundle for Germany failed since Italy have previously failed and thus the misses cache
         // contained a false entry
+
+        // Set default Locale to Locale.US
+        Locale defaultLocale = Locale.getDefault();
+        Locale.setDefault(Locale.US);
+
         ResourceBundle rbFrance = LocalizedTextUtil.findResourceBundle("com/opensymphony/xwork2/util/XW404", Locale.FRANCE);
         ResourceBundle rbItaly = LocalizedTextUtil.findResourceBundle("com/opensymphony/xwork2/util/XW404", Locale.ITALY);
         ResourceBundle rbGermany = LocalizedTextUtil.findResourceBundle("com/opensymphony/xwork2/util/XW404", Locale.GERMANY);
+
+        // Reset to previous default Locale 
+        Locale.setDefault(defaultLocale);
 
         assertNotNull(rbFrance);
         assertEquals("Bonjour", rbFrance.getString("hello"));
