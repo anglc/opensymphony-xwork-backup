5b456d75603c81e82504ecfe6703adc8818c61e4
tm_jee <tm_jee@8c1e88eb-08f3-148f-2d68-3308c1931f23>
2008-01-19 10:44:21 +0000

    Removed wrongly added directory

--- xwork-svn2/docs/wikidocs/README.txt
+++
@@ -1,2 +0,0 @@
-Documentation of XWork 1.2.x have been merged with WebWork 2.2.x, hence the docs for XWork 1.2.x can be found with WebWork 2.2.x distribution.
-
--- xwork-svn2/tiger/src/java/com/opensymphony/xwork/config/annotations/InterceptorRef.java
+++
@@ -1,9 +0,0 @@
-package com.opensymphony.xwork.config.annotations;
-
-import java.lang.annotation.Retention;
-import java.lang.annotation.RetentionPolicy;
-
-@Retention(RetentionPolicy.RUNTIME)
-public @interface InterceptorRef {
-  String value();
-}
