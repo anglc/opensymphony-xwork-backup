f80b24d0b33aea42d24b3c1c43136920e1f335c7
tm_jee <tm_jee@8c1e88eb-08f3-148f-2d68-3308c1931f23>
2008-01-19 10:41:48 +0000

    Do a XWork_1-2-4 tag

---
+++ tiger/src/test/com/opensymphony/xwork/test/AnnotationUserMarker.java
@@ -0,0 +1,27 @@
+/*
+ * Copyright (c) 2002-2006 by OpenSymphony
+ * All rights reserved.
+ */
+package com.opensymphony.xwork.test;
+
+import com.opensymphony.xwork.validator.annotations.*;
+
+/**
+ * Marker interface to help test hierarchy traversal.
+ *
+ * @author Mark Woon
+ * @author Rainer Hermanns
+ */
+@Validation(
+        validations = @Validations(
+                requiredFields = {
+                    @RequiredFieldValidator(fieldName = "email", shortCircuit = true, message = "You must enter a value for email."),
+                    @RequiredFieldValidator(fieldName = "email2", shortCircuit = true, message = "You must enter a value for email2.")
+                },
+                expressions = {
+                        @ExpressionValidator(shortCircuit = true, expression = "email.equals(email2)", message = "Email not the same as email2" )
+                }
+        )
+)
+public interface AnnotationUserMarker {
+}
