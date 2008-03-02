1f50fcc9f101a31c77253b5dd46cdf342aaa3cf9
rainerh <rainerh@8c1e88eb-08f3-148f-2d68-3308c1931f23>
2008-03-02 14:50:36 +0000

    XW-596 prepareInterceptor doesn't throw any exception when only using prepare{method} o applied suggested patch supplied by Bart Verwiere

--- src/java/com/opensymphony/xwork2/interceptor/PrepareInterceptor.java
+++ src/java/com/opensymphony/xwork2/interceptor/PrepareInterceptor.java
@@ -9,6 +9,8 @@ import com.opensymphony.xwork2.Preparable;
 import com.opensymphony.xwork2.util.logging.Logger;
 import com.opensymphony.xwork2.util.logging.LoggerFactory;
 
+import java.lang.reflect.InvocationTargetException;
+
 
 /**
  * <!-- START SNIPPET: description -->
@@ -16,7 +18,7 @@ import com.opensymphony.xwork2.util.logging.LoggerFactory;
  * This interceptor calls <code>prepare()</code> on actions which implement
  * {@link Preparable}. This interceptor is very useful for any situation where
  * you need to ensure some logic runs before the actual execute method runs.
- * 
+ *
  * <p/> A typical use of this is to run some logic to load an object from the
  * database so that when parameters are set they can be set on this object. For
  * example, suppose you have a User object with two properties: <i>id</i> and
@@ -25,7 +27,7 @@ import com.opensymphony.xwork2.util.logging.LoggerFactory;
  * the id property, and then when the second params interceptor is called the
  * parameter <i>user.name</i> will be set, as desired, on the actual object
  * loaded from the database. See the example for more info.
- * 
+ *
  * <p/>
  * <b>Note:</b> Since XWork 2.0.2, this interceptor extends {@link MethodFilterInterceptor}, therefore being
  * able to deal with excludeMethods / includeMethods parameters. See [Workflow Interceptor]
@@ -33,7 +35,7 @@ import com.opensymphony.xwork2.util.logging.LoggerFactory;
  *
  * <p/><b>Update</b>: Added logic to execute a prepare{MethodName} and conditionally
  * the a general prepare() Method, depending on the 'alwaysInvokePrepare' parameter/property
- * which is by default true. This allows us to run some logic based on the method 
+ * which is by default true. This allows us to run some logic based on the method
  * name we specify in the {@link com.opensymphony.xwork2.ActionProxy}. For example, you can specify a
  * prepareInput() method that will be run before the invocation of the input method.
  *
@@ -45,7 +47,7 @@ import com.opensymphony.xwork2.util.logging.LoggerFactory;
  *
  * <ul>
  *
- * <li>alwaysInvokePrepare - Default to true. If true, prepare will always be invoked, 
+ * <li>alwaysInvokePrepare - Default to true. If true, prepare will always be invoked,
  * otherwise it will not.</li>
  *
  * </ul>
@@ -83,40 +85,50 @@ import com.opensymphony.xwork2.util.logging.LoggerFactory;
  * @see com.opensymphony.xwork2.Preparable
  */
 public class PrepareInterceptor extends MethodFilterInterceptor {
-	
-	private static final long serialVersionUID = -5216969014510719786L;
 
-	private static final Logger LOG = LoggerFactory.getLogger(PrepareInterceptor.class);
-	
-	private final static String PREPARE_PREFIX = "prepare";
-	private final static String ALT_PREPARE_PREFIX = "prepareDo";
+    private static final long serialVersionUID = -5216969014510719786L;
 
-	private boolean alwaysInvokePrepare = true;
+    private static final Logger LOG = LoggerFactory.getLogger(PrepareInterceptor.class);
+
+    private final static String PREPARE_PREFIX = "prepare";
+    private final static String ALT_PREPARE_PREFIX = "prepareDo";
+
+    private boolean alwaysInvokePrepare = true;
 
     /**
      * Sets if the <code>preapare</code> method should always be executed.
      * <p/>
      * Default is <tt>true</tt>.
      *
-     * @param alwaysInvokePrepare  if <code>prepare</code> should always be executed or not.
+     * @param alwaysInvokePrepare if <code>prepare</code> should always be executed or not.
      */
     public void setAlwaysInvokePrepare(String alwaysInvokePrepare) {
-		this.alwaysInvokePrepare = Boolean.parseBoolean(alwaysInvokePrepare);
-	}
-	
+        this.alwaysInvokePrepare = Boolean.parseBoolean(alwaysInvokePrepare);
+    }
+
     public String doIntercept(ActionInvocation invocation) throws Exception {
         Object action = invocation.getAction();
 
         if (action instanceof Preparable) {
             try {
                 PrefixMethodInvocationUtil.invokePrefixMethod(invocation,
-                    new String[] { PREPARE_PREFIX, ALT_PREPARE_PREFIX });
+                        new String[]{PREPARE_PREFIX, ALT_PREPARE_PREFIX});
             }
-            catch(Exception e) {
+            catch (InvocationTargetException e) {
                 // just in case there's an exception while doing reflection,
                 // we still want prepare() to be able to get called.
                 LOG.warn("an exception occured while trying to execute prefixed method", e);
             }
+            catch (IllegalAccessException e) {
+                // just in case there's an exception while doing reflection,
+                // we still want prepare() to be able to get called.
+                LOG.warn("an exception occured while trying to execute prefixed method", e);
+            } catch (Exception e) {
+                // just in case there's an exception while doing reflection,
+                // we still want prepare() to be able to get called.
+                LOG.warn("an exception occured while trying to execute prefixed method", e);
+            }
+
             if (alwaysInvokePrepare) {
                 ((Preparable) action).prepare();
             }
