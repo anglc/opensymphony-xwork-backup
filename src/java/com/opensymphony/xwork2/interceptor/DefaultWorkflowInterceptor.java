cc4754b0ed350fbce4a2d49b4de627dd75bc507a
rainerh <rainerh@8c1e88eb-08f3-148f-2d68-3308c1931f23>
2008-06-11 14:56:46 +0000

    XW-608 Workflow Interceptor should allow the action to determine the input view name o applied patch by Sami Dalouche

--- src/java/com/opensymphony/xwork2/interceptor/DefaultWorkflowInterceptor.java
+++ src/java/com/opensymphony/xwork2/interceptor/DefaultWorkflowInterceptor.java
@@ -12,59 +12,59 @@ import com.opensymphony.xwork2.util.logging.LoggerFactory;
 
 /**
  * <!-- START SNIPPET: description -->
- *
+ * <p/>
  * An interceptor that makes sure there are not validation errors before allowing the interceptor chain to continue.
  * <b>This interceptor does not perform any validation</b>.
- *
+ * <p/>
  * <p/>This interceptor does nothing if the name of the method being invoked is specified in the <b>excludeMethods</b>
  * parameter. <b>excludeMethods</b> accepts a comma-delimited list of method names. For example, requests to
  * <b>foo!input.action</b> and <b>foo!back.action</b> will be skipped by this interceptor if you set the
  * <b>excludeMethods</b> parameter to "input, back".
- *
+ * <p/>
  * <b>Note:</b> As this method extends off MethodFilterInterceptor, it is capable of
  * deciding if it is applicable only to selective methods in the action class. This is done by adding param tags
  * for the interceptor element, naming either a list of excluded method names and/or a list of included method
  * names, whereby includeMethods overrides excludedMethods. A single * sign is interpreted as wildcard matching
  * all methods for both parameters.
  * See {@link MethodFilterInterceptor} for more info.
- *
+ * <p/>
  * <!-- END SNIPPET: description -->
- *
+ * <p/>
  * <p/> <u>Interceptor parameters:</u>
- *
+ * <p/>
  * <!-- START SNIPPET: parameters -->
- *
+ * <p/>
  * <ul>
- *
+ * <p/>
  * <li>inputResultName - Default to "input". Determine the result name to be returned when
  * an action / field error is found.</li>
- *
- * </ul>
- *
- * <!-- END SNIPPET: parameters -->
- *
- * <p/> <u>Extending the interceptor:</u>
- *
  * <p/>
- *
+ * </ul>
+ * <p/>
+ * <!-- END SNIPPET: parameters -->
+ * <p/>
+ * <p/> <u>Extending the interceptor:</u>
+ * <p/>
+ * <p/>
+ * <p/>
  * <!-- START SNIPPET: extending -->
- *
+ * <p/>
  * There are no known extension points for this interceptor.
- *
+ * <p/>
  * <!-- END SNIPPET: extending -->
- *
+ * <p/>
  * <p/> <u>Example code:</u>
- *
+ * <p/>
  * <pre>
  * <!-- START SNIPPET: example -->
- * 
+ * <p/>
  * &lt;action name="someAction" class="com.examples.SomeAction"&gt;
  *     &lt;interceptor-ref name="params"/&gt;
  *     &lt;interceptor-ref name="validation"/&gt;
  *     &lt;interceptor-ref name="workflow"/&gt;
  *     &lt;result name="success"&gt;good_result.ftl&lt;/result&gt;
  * &lt;/action&gt;
- * 
+ * <p/>
  * &lt;-- In this case myMethod as well as mySecondMethod of the action class
  *        will not pass through the workflow process --&gt;
  * &lt;action name="someAction" class="com.examples.SomeAction"&gt;
@@ -75,7 +75,7 @@ import com.opensymphony.xwork2.util.logging.LoggerFactory;
  *     &lt;/interceptor-ref name="workflow"&gt;
  *     &lt;result name="success"&gt;good_result.ftl&lt;/result&gt;
  * &lt;/action&gt;
- *
+ * <p/>
  * &lt;-- In this case, the result named "error" will be used when
  *        an action / field error is found --&gt;
  * &lt;-- The Interceptor will only be applied for myWorkflowMethod method of action
@@ -85,12 +85,12 @@ import com.opensymphony.xwork2.util.logging.LoggerFactory;
  *     &lt;interceptor-ref name="validation"/&gt;
  *     &lt;interceptor-ref name="workflow"&gt;
  *        &lt;param name="inputResultName"&gt;error&lt;/param&gt;
-*         &lt;param name="excludeMethods"&gt;*&lt;/param&gt;
-*         &lt;param name="includeMethods"&gt;myWorkflowMethod&lt;/param&gt;
+ *         &lt;param name="excludeMethods"&gt;*&lt;/param&gt;
+ *         &lt;param name="includeMethods"&gt;myWorkflowMethod&lt;/param&gt;
  *     &lt;/interceptor-ref&gt;
  *     &lt;result name="success"&gt;good_result.ftl&lt;/result&gt;
  * &lt;/action&gt;
- *
+ * <p/>
  * <!-- END SNIPPET: example -->
  * </pre>
  *
@@ -101,40 +101,46 @@ import com.opensymphony.xwork2.util.logging.LoggerFactory;
  * @author tm_jee
  */
 public class DefaultWorkflowInterceptor extends MethodFilterInterceptor {
-	
-	private static final long serialVersionUID = 7563014655616490865L;
 
-	private static final Logger LOG = LoggerFactory.getLogger(DefaultWorkflowInterceptor.class);
-	
-	private String inputResultName = Action.INPUT;
-	
-	/**
-	 * Set the <code>inputResultName</code> (result name to be returned when 
-	 * a action / field error is found registered). Default to {@link Action#INPUT}
-	 * 
-	 * @param inputResultName what result name to use when there was validation error(s).
-	 */
-	public void setInputResultName(String inputResultName) {
-		this.inputResultName = inputResultName;
-	}
-	
-	/**
-	 * Intercept {@link ActionInvocation} and returns a <code>inputResultName</code>
-	 * when action / field errors is found registered.
-	 * 
-	 * @return String result name
-	 */
+    private static final long serialVersionUID = 7563014655616490865L;
+
+    private static final Logger LOG = LoggerFactory.getLogger(DefaultWorkflowInterceptor.class);
+
+    private String inputResultName = Action.INPUT;
+
+    /**
+     * Set the <code>inputResultName</code> (result name to be returned when
+     * a action / field error is found registered). Default to {@link Action#INPUT}
+     *
+     * @param inputResultName what result name to use when there was validation error(s).
+     */
+    public void setInputResultName(String inputResultName) {
+        this.inputResultName = inputResultName;
+    }
+
+    /**
+     * Intercept {@link ActionInvocation} and returns a <code>inputResultName</code>
+     * when action / field errors is found registered.
+     *
+     * @return String result name
+     */
     protected String doIntercept(ActionInvocation invocation) throws Exception {
         Object action = invocation.getAction();
-        
+
         if (action instanceof ValidationAware) {
             ValidationAware validationAwareAction = (ValidationAware) action;
 
             if (validationAwareAction.hasErrors()) {
-            	if (LOG.isDebugEnabled()) {
-            		LOG.debug("Errors on action "+validationAwareAction+", returning result name 'input'");
-            	}
-            	return inputResultName;
+                if (LOG.isDebugEnabled()) {
+                    LOG.debug("Errors on action " + validationAwareAction + ", returning result name 'input'");
+                }
+
+                String resultName = inputResultName;
+                
+                if (action instanceof ValidationWorkflowAware) {
+                    resultName = ((ValidationWorkflowAware) action).getInputResultName();
+                }
+                return resultName;
             }
         }
 
