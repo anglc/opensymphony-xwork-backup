5d0194c945f874ccc05580bdd0fdf848885c183f
rainerh <rainerh@8c1e88eb-08f3-148f-2d68-3308c1931f23>
2008-06-12 13:57:47 +0000

    XW-535 external-ref tag's content cannot be read occasionally by some parser o ported fix from xwork 1.2 branch

--- src/java/com/opensymphony/xwork2/config/providers/XmlHelper.java
+++ src/java/com/opensymphony/xwork2/config/providers/XmlHelper.java
@@ -19,6 +19,27 @@ import java.util.LinkedHashMap;
  */
 public class XmlHelper {
 
+
+    /**
+     * This method will find all the parameters under this <code>paramsElement</code> and return them as
+     * Map<String, String>. For example,
+     * <pre>
+     *   <result ... >
+     *      <param name="param1">value1</param>
+     *      <param name="param2">value2</param>
+     *      <param name="param3">value3</param>
+     *   </result>
+     * </pre>
+     * will returns a Map<String, String> with the following key, value pairs :-
+     * <ul>
+     * <li>param1 - value1</li>
+     * <li>param2 - value2</li>
+     * <li>param3 - value3</li>
+     * </ul>
+     *
+     * @param paramsElement
+     * @return
+     */
     public static Map getParams(Element paramsElement) {
         LinkedHashMap params = new LinkedHashMap();
 
@@ -35,23 +56,44 @@ public class XmlHelper {
                 Element paramElement = (Element) childNode;
                 String paramName = paramElement.getAttribute("name");
 
-                StringBuffer paramValue = new StringBuffer();
-                for (int j=0; j <paramElement.getChildNodes().getLength(); j++) {
-                	if (paramElement.getChildNodes().item(j) != null && 
-                			paramElement.getChildNodes().item(j).getNodeType() == Node.TEXT_NODE) {
-                		String val = paramElement.getChildNodes().item(j).getNodeValue();
-                		if (val != null) {
-                			paramValue .append(val.trim());
-                		} 
-                	}
-                }
-                String val = paramValue.toString().trim();
+                String val = getContent(paramElement);
                 if (val.length() > 0) {
-                	params.put(paramName, val);
+                    params.put(paramName, val);
                 }
             }
         }
 
         return params;
     }
+
+    /**
+     * This method will return the content of this particular <code>element</code>.
+     * For example,
+     * <p/>
+     * <pre>
+     *    <result>something_1</result>
+     * </pre>
+     * When the {@link org.w3c.dom.Element} <code>&lt;result&gt;</code> is passed in as
+     * argument (<code>element</code> to this method, it returns the content of it,
+     * namely, <code>something_1</code> in the example above.
+     *
+     * @return
+     */
+    public static String getContent(Element element) {
+        StringBuffer paramValue = new StringBuffer();
+        NodeList childNodes = element.getChildNodes();
+        for (int j = 0; j < childNodes.getLength(); j++) {
+            Node currentNode = childNodes.item(j);
+            if (currentNode != null &&
+                    currentNode.getNodeType() == Node.TEXT_NODE) {
+                String val = currentNode.getNodeValue();
+                if (val != null) {
+                    paramValue.append(val.trim());
+                }
+            }
+        }
+        String val = paramValue.toString().trim();
+        return val;
+    }
+
 }
