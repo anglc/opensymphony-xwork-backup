09029792adc72a1ed721d40e57a699ac6c708e72
musachy <musachy@8c1e88eb-08f3-148f-2d68-3308c1931f23>
2008-12-09 18:46:38 +0000

    Fix javadoc to make it clear that the request parameters are the ones that will be blocked

--- src/java/com/opensymphony/xwork2/interceptor/NoParameters.java
+++ src/java/com/opensymphony/xwork2/interceptor/NoParameters.java
@@ -9,7 +9,7 @@ package com.opensymphony.xwork2.interceptor;
  * Marker interface to incidate no auto setting of parameters.
  * <p/>
  * This marker interface should be implemented by actions that do not want any
- * parameters set on them automatically (by the ParametersActionFactoryProxy).
+ * request parameters set on them automatically (by the ParametersInterceptor).
  * This may be useful if one is using the action tag and want to supply
  * the parameters to the action manually using the param tag.
  * It may also be useful if one for security reasons wants to make sure that
